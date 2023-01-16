c94e2f4f127ab1570fb55787b4c32844e2139e0f
aali <aali@wideagency.com>
2023-01-16 10:53:00 +0100

    dev: added static server

--- back/server.js
+++ back/server.js
@@ -1,1 +1,19 @@
 console.log("test");
+
+const express = require("express");
+const serveIndex = require("serve-index");
+
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log("req: ", req.url);
+  next();
+});
+
+app.use(express.static("."));
+app.use(serveIndex(".", { icons: true }));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
