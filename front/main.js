54e4e1e9eb0d27ea352a9857235a76d72aea7a30
aali <aali@wideagency.com>
2023-01-16 14:46:03 +0100

    dev: started front

---
+++ front/main.js
@@ -0,0 +1,28 @@
+const svgns = "http://www.w3.org/2000/svg";
+
+const r = 1;
+const r0 = 45;
+const cx0 = 50;
+const cy0 = 50;
+
+const getAngleFromIndex = (index) => {
+  return (index * 2 * Math.PI) / 10;
+};
+
+const getPointFromAngle = (angle) => {
+  return {
+    x: cx0 + r0 * Math.cos(angle),
+    y: cy0 + r0 * Math.sin(angle),
+  };
+};
+
+const container = document.querySelector("g.samples");
+for (let i = 0; i < 10; i++) {
+  const angle = getAngleFromIndex(i);
+  const { x: cx, y: cy } = getPointFromAngle(angle);
+  const circle = document.createElementNS(svgns, "circle");
+  circle.setAttributeNS(null, "cx", cx);
+  circle.setAttributeNS(null, "cy", cy);
+  circle.setAttributeNS(null, "r", r);
+  container.appendChild(circle);
+}
